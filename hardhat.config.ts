c3d48015330e060b0d2e88c5e895d80bf540fe72
hyunchul jung <blackironj@gmail.com>
2022-03-22 10:35:16 +0900

    update deployment script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,11 @@
+require('dotenv').config();
+
 import '@typechain/hardhat'
 import '@nomiclabs/hardhat-ethers'
 import '@nomiclabs/hardhat-waffle';
 import '@nomiclabs/hardhat-solhint';
-import { task } from 'hardhat/config';
+
+import { task, HardhatUserConfig } from 'hardhat/config';
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -20,6 +23,14 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-module.exports = {
-  solidity: "0.8.4",
+const config: HardhatUserConfig = {
+  solidity: '0.8.4',
+  networks: {
+    ropsten: {
+      url: process.env.ROPSTEN_RUL || '',
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
 };
+
+export default config;
