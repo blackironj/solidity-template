93ea040dac96accad359a8a8931210b197f7ada4
hyunchul jung <blackironj@gmail.com>
2022-03-22 16:17:25 +0900

    added: matic testnet to hardhat.config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -27,8 +27,14 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 const config: HardhatUserConfig = {
   solidity: '0.8.4',
   networks: {
+    //eth testnet
     ropsten: {
-      url: process.env.ROPSTEN_RUL || '',
+      url: process.env.ROPSTEN_URL || '',
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    //matic testnet
+    mumbai: {
+      url: process.env.MUMBAI_URL || '',
       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
   },
